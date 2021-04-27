021779f0b0188ca11c8ab179678f61338aa6cf52
Sergei Osokin <sergei.osokin.rf@mail.ru>
2021-04-27 20:51:04 +0300

    Updates

--- src/redux/types.js
+++
@@ -1,6 +0,0 @@
-export const CREATE_POST = 'POST/CREATE_POST';
-export const SERVER_POSTS = 'POST/SERVER_POST';
-export const NEW_INFO = 'PROFILE/NEW_INFO';
-// export const HIDE_LOADER = 'APP/HIDE_LOADER';
-// export const SHOW_ALERT = 'APP/ALERT_LOADER';
-// export const HIDE_ALERT = 'APP/ALERT_LOADER';
